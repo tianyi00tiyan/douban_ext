9471ee53f07124688785db12297de01593b4a565
t.zhou <tao.zhou@starscape.live>
2020-10-26 10:49:31 +0800

    u

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 /**
  * 需要在后台设计记录当前配置状态
  */
-var HD = 1; //高清大图
-var FHD = 2; //全高清大图
-var curRatio = HD; //图片质量
+// var HD = 1; //高清大图
+// var FHD = 2; //全高清大图
+// var curRatio = HD; //图片质量
 var canUse = true; //是否使用插件
 
 /**
@@ -20,12 +20,12 @@ function bindEvent() {
       sendMessage('BG.canUse', canUse);
 
       //通知给前台当前curRatio状态
-      sendMessage('BG.curRatio', curRatio);
+      // sendMessage('BG.curRatio', curRatio);
 
       //通知加载完毕
       sendMessage('BG.onCompleted');
     },
-    { urls: ['https://item.jd.com/100012014970.html'] },
+    { urls: ['https://item.jd.com/100012014970.html', 'https://trade.jd.com/shopping/*'] },
   );
 }
 
@@ -34,12 +34,18 @@ function bindEvent() {
  */
 function sendMessage(type, value, allTabs) {
   if (!allTabs) {
-    //是否所有的tab都要通知
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { type: type, value: value });
+    chrome.tabs.query({ active: true }, function (tabs) {
+      console.log(tabs);
+      if (tabs && tabs.length) {
+        console.log(tabs[0].id);
+        console.log({ type: type, value: value });
+        setTimeout(() => {
+          chrome.tabs.sendMessage(tabs[0].id, { type: type, value: value });
+        }, 500);
+      }
     });
   } else {
-    chrome.tabs.query({ url: 'https://www.douban.com/gallery/topic/*' }, function (tabs) {
+    chrome.tabs.query({ url: 'https://item.jd.com/*' }, function (tabs) {
       for (let index = 0; index < tabs.length; index++) {
         chrome.tabs.sendMessage(tabs[index].id, { type: type, value: value });
       }
